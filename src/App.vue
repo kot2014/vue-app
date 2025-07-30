81f882a88ec26b65d3f8f839c591c56ccf4f43ac
Yaroslav Mostovoy <mostovy.yaroslav.igorovich@gmail.com>
2025-07-31 00:48:12 +0300

    add 2 button, and header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <DefaultLayout>
+    <HomeView />
+  </DefaultLayout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DefaultLayout from './layouts/DefaultLayout.vue'
+import HomeView from './views/HomeView.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DefaultLayout,
+    HomeView
   }
 }
 </script>
@@ -21,6 +24,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+body {
+  margin: 0;
 }
 </style>
---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <HeaderComponent />
+    <slot />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from '../components/HeaderComponent.vue'
+// import Footer from '../components/Footer.vue'
+
+export default {
+  components: {
+    HeaderComponent,
+    // Footer
+  }
+}
+</script>
